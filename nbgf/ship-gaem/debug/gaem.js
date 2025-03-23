d88589fd077547ef141406c9c0079f9643318591
albertvaka <albertvaka@gmail.com>
2025-03-23 15:08:03 +0000

    Deploying to main from @ albertvaka/nbgf@ff8f224375c1c93af70d884b4dcd016dc55dd187 🚀

--- nbgf/ship-gaem/debug/gaem.js
+++ nbgf/ship-gaem/debug/gaem.js
@@ -31,7 +31,7 @@ if (ENVIRONMENT_IS_NODE) {
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /tmp/tmp88zyov9h.js
+// include: /tmp/tmpspu857nm.js
 
   Module['expectedDataFileDownloads'] ??= 0;
   Module['expectedDataFileDownloads']++;
@@ -215,7 +215,7 @@ Module['FS_createPath']("/", "data", true, true);
 
   })();
 
-// end include: /tmp/tmp88zyov9h.js
+// end include: /tmp/tmpspu857nm.js
 
 
 // Sometimes an existing Module object exists with properties
