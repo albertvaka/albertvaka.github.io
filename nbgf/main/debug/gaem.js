95afc539cc3ff3cd8675efac07f2e8c52b76bb92
albertvaka <albertvaka@gmail.com>
2025-12-27 20:29:01 +0000

    Deploying to main from @ albertvaka/nbgf@281b8d3be75e8f982b5495b092fbc13e4df0665c 🚀

--- nbgf/main/debug/gaem.js
+++ nbgf/main/debug/gaem.js
@@ -29,7 +29,7 @@ var ENVIRONMENT_IS_SHELL = !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_NODE && !ENVIR
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /tmp/tmpzgsp5o2r.js
+// include: /tmp/tmp3knl9zt7.js
 
   if (!Module['expectedDataFileDownloads']) Module['expectedDataFileDownloads'] = 0;
   Module['expectedDataFileDownloads']++;
@@ -168,7 +168,7 @@ Module['FS_createPath']("/", "data", true, true);
 
   })();
 
-// end include: /tmp/tmpzgsp5o2r.js
+// end include: /tmp/tmp3knl9zt7.js
 
 
 var arguments_ = [];
